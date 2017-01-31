be5576c344fa590e436b6796508f143500ce2d13
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2017-01-31 13:05:45 +0100

    LCD fix 1

--- Score Code/MicroOSUScore.c
+++ Score Code/MicroOSUScore.c
@@ -1,3 +1,4 @@
+// LCD display configuration
 sbit LCD_RS at RB4_bit; // RS på LCD: n ansluts till B4 på PIC:en
 sbit LCD_EN at RB5_bit; // E(Enable på LCD: n ansluts till B5 på PIC:en
 
@@ -6,7 +7,6 @@ sbit LCD_D5 at RB1_bit; // D5 på LCD: n ansluts till B1 på PIC:en
 sbit LCD_D6 at RB2_bit; // D6 på LCD: n ansluts till B2 på PIC:en
 sbit LCD_D7 at RB3_bit; // D7 på LCD: n ansluts till B3 på PIC:en
 
-// Tell mirco processor how it is connected to the LCD display
 sbit LCD_RS_Direction at TRISB4_bit;
 sbit LCD_EN_Direction at TRISB5_bit;
 sbit LCD_D4_Direction at TRISB0_bit;
@@ -20,6 +20,7 @@ void update();
 
 void main() {
    initialize();
+   update();
 }
 
 void initialize() {
@@ -29,7 +30,7 @@ void initialize() {
    ANSEL = 0b00000000;             // No analog inputs
    ANSELH = 0b00000000;            // No analog inputs
 
-   TRISA = 0b11111111;
+   TRISA = 0b00000000;
    TRISB = 0b00000000;
    TRISC = 0b00000000;
    PORTA = 0b00000000;
@@ -41,9 +42,9 @@ void initialize() {
    Lcd_Cmd(_LCD_CLEAR);
    Lcd_Cmd(_LCD_CURSOR_OFF);
 
-   Lcd_Rs = 0;
+   LCD_RS = 0;
    Lcd_Cmd(64);
-   Lcd_Rs = 1;
+   LCD_RS = 1;
 
    LCD_Chr_Cp(0b00000000);
    LCD_Chr_Cp(0b00001110);
@@ -54,11 +55,32 @@ void initialize() {
    LCD_Chr_Cp(0b00000000);
    LCD_Chr_Cp(0b00000000);
 
-   Lcd_Rs = 1;
+   LCD_RS = 0;
+   LCD_Cmd(128);
+   LCD_RS = 1;
+
+   Lcd_Out(1, 1, "Score: ");
+   Lcd_Out(1, 8, "1337");
+
 }
 
 void update() {
-  while(1) {
+  /*while(1) {
     LCD_Chr(2, 2, 0);
+  }*/
+  char i;
+  Lcd_Out(1, 1, "HEJ OCH ");
+  Lcd_Chr_Cp(0);
+  for(i=0; i<5; i++) {
+      LCD_Chr(2, i+1, 49+i);
+  }
+  for(i=0; i<5; i++) {
+      LCD_Chr(3, 1+i, 65+i);
+  }
+  while(1==1) {
+     for(i=0; i<16; i++) {
+         Lcd_Cmd( (i<=7) ? _LCD_SHIFT_RIGHT : _LCD_SHIFT_LEFT);
+         Delay_ms(500);
+     }
   }
 }
